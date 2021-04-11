393e020cee57b0798f76546938a73568fd3a36ce
rikardbq <j10rikardb@gmail.com>
2021-04-11 17:29:23 +0200

    add swing_damage and range_damage to event filter

--- OwenWillWow.lua
+++ OwenWillWow.lua
@@ -11,12 +11,12 @@ f:SetScript(
 
       local playerGUID = UnitGUID("player");
 
-      if (sourceGUID == playerGUID and string.match(type, "SPELL_") ~= nil) then
+      if sourceGUID == playerGUID and (type:match("SPELL_") ~= nil or type:match("SWING") ~= nil or type:match("RANGE") ~= nil) then
         local critical
-        if string.match(type, "_DAMAGE") ~= nil then 
-          critical = select(21, ...)
-        elseif string.match(type, "_HEAL") ~= nil then
+        if type:match("_HEAL") ~= nil or type:match("SWING_DAMAGE") then
           critical = select(18, ...)
+        elseif type:match("_DAMAGE") ~= nil then
+          critical = select(21, ...)
         end
         
         if (critical) then
